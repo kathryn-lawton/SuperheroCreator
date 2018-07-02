dd07546aab89491d044f46699a3ae0c82692b848
Kathryn Lawton <klawton16@gmail.com>
2018-07-02 09:09:05 -0500

    adding initial files

---
+++ SuperheroCreator/Models/Superhero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperheroCreator.Models
+{
+	public class Superhero
+	{
+		[Key]
+		public int SuperheroId { get; set; }
+		public string Name { get; set; }
+		public string AlterEgoName { get; set; }
+		public string PrimaryAbility { get; set; }
+		public string SecondaryAbility { get; set; }
+		public string Catchphrase { get; set; }
+
+	}
+}
