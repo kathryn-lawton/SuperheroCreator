98ced59395982e2d655fce57a3e4b2b5079491d7
Kathryn Lawton <klawton16@gmail.com>
2018-07-03 08:01:00 -0500

    added create, edit, and detail actions

--- SuperheroCreator/Controllers/SuperheroController.cs
+++ SuperheroCreator/Controllers/SuperheroController.cs
@@ -13,8 +13,88 @@ namespace SuperheroCreator.Controllers
 		public ActionResult Index()
         {
 			ApplicationDbContext db = new ApplicationDbContext();
+			var model = db.Superhero.ToList();
 
-            return View();
+            return View(model);
         }
+
+		[HttpGet]
+		public ActionResult Create()
+		{
+			ApplicationDbContext db = new ApplicationDbContext();
+			
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create([Bind (Include = "SuperheroId, Name, AlterEgoName, PrimaryAbility, SecondaryAbility, Catchphrase")] Superhero superhero)
+		{
+			ApplicationDbContext db = new ApplicationDbContext();
+			if (ModelState.IsValid)
+			{
+				db.Superhero.Add(superhero);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			ViewBag.superheroId = new SelectList(db.Superhero, "Name", "AlterEgoName", "PrimaryAbility", "SecondaryAbility", "Catchphrase");
+			return View(superhero);
+		}
+
+		[HttpGet]
+		public ActionResult Details(int? id)
+		{
+			if(id == null)
+			{
+				return RedirectToAction("Index");
+			}
+
+			ApplicationDbContext db = new ApplicationDbContext();
+
+			var foundSuperhero = db.Superhero.Where(s => s.SuperheroId == id).FirstOrDefault();
+			if (foundSuperhero == null)
+			{
+				return RedirectToAction("Index");
+			}
+			return View(foundSuperhero);
+		}
+
+		[HttpGet]
+		public ActionResult Edit(int? id)
+		{
+			if(id == null)
+			{
+				return RedirectToAction("Index");
+			}
+			ApplicationDbContext db = new ApplicationDbContext();
+	
+			var foundSuperheroId = db.Superhero.Where(s => s.SuperheroId == id).FirstOrDefault();
+			if (foundSuperheroId == null) 
+			{
+				return RedirectToAction("Index");
+			}
+			return View(foundSuperheroId);
+		}
+
+		[HttpPost]
+		public ActionResult Edit([Bind(Include = "SuperheroId, Name, AlterEgoName, PrimaryAbility, SecondaryAbility, Catchphrase")] Superhero superhero)
+		{
+			ApplicationDbContext db = new ApplicationDbContext();
+			try
+			{
+				if(ModelState.IsValid)
+				{
+					db.Entry(superhero).State = System.Data.Entity.EntityState.Modified;
+					db.SaveChanges();
+					return RedirectToAction("Index");
+				}
+			}
+			catch (Exception)
+			{
+				ModelState.AddModelError("", "Unable to save changes. Please try again, and if the problem continues contact your system administrator.");
+			}
+			return View(superhero);
+		}
+
+
     }
 }
